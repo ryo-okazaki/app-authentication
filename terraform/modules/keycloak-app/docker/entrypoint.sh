92b665aa39578497ed1739745a6eec5df6d04eea
Ryo Okazaki <okazaki3905@gmail.com>
2025-12-27 09:58:03 +0900

    fix: keycloak startup method

--- terraform/modules/keycloak-app/docker/entrypoint.sh
+++ terraform/modules/keycloak-app/docker/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-envsubst < /opt/keycloak/data/import/master-realm-terraform.json.template \
-         > /opt/keycloak/data/import/master-realm-terraform.json
-
 exec /opt/keycloak/bin/kc.sh "$@"
